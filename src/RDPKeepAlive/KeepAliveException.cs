3f333b9dffb408378cdc8a2fa913d79daecf1091
apls <zpapls@gmail.com>
2026-05-27 09:57:39 +0800

    Refactor KeepAliveException constructors for nullability

--- src/RDPKeepAlive/KeepAliveException.cs
+++ src/RDPKeepAlive/KeepAliveException.cs
@@ -1,18 +1,22 @@
-﻿using System;
+using System;
+using System.Runtime.InteropServices;
 
 namespace RDPKeepAlive
 {
     public class KeepAliveException : Exception
     {
-        public KeepAliveException() : base()
+        public KeepAliveException()
+            : base()
         {
         }
 
-        public KeepAliveException(string? message) : base(message)
+        public KeepAliveException(string message)
+            : base(message)
         {
         }
 
-        public KeepAliveException(string? message, Exception? innerException) : base(message, innerException)
+        public KeepAliveException(string message, Exception innerException)
+            : base(message, innerException)
         {
         }
     }
