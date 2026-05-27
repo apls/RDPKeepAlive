5f4196f8241e1d44a5b78dac8bd3b778ab3017f3
apls <zpapls@gmail.com>
2026-05-27 09:55:28 +0800

    Refactor Monitor class for improved clarity and structure

--- src/RDPKeepAlive/Monitor.cs
+++ src/RDPKeepAlive/Monitor.cs
@@ -1,8 +1,6 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace RDPKeepAlive
@@ -15,35 +13,39 @@ namespace RDPKeepAlive
             {
                 var closure = new MonitorEnumCallback();
                 var proc = new MonitorEnumProc(closure.Callback);
-                _ = NativeMethods.EnumDisplayMonitors(HandleRef, IntPtr.Zero, proc, IntPtr.Zero);
-                return closure.Monitors.Cast<Monitor>().ToList();
+                NativeMethods.EnumDisplayMonitors(HandleRef, IntPtr.Zero, proc, IntPtr.Zero);
+                var result = new List<Monitor>();
+                foreach (Monitor m in closure.Monitors)
+                {
+                    result.Add(m);
+                }
+                return result;
             }
         }
 
-        internal Rect Bounds { get; }
+        internal Rect Bounds { get; private set; }
+        internal IntPtr Handle { get; private set; }
+        internal bool IsPrimary { get; private set; }
+        internal string Name { get; private set; }
+        internal Rect WorkingArea { get; private set; }
 
-        internal IntPtr Handle { get; }
+        private static readonly HandleRef HandleRef = new HandleRef(null, IntPtr.Zero);
 
-        internal bool IsPrimary { get; }
-
-        internal string Name { get; }
-
-        internal Rect WorkingArea { get; }
-
-        private static readonly HandleRef HandleRef = new(null, IntPtr.Zero);
-
-        private Monitor(IntPtr monitor, IntPtr? hdc)
+        private Monitor(IntPtr monitor, IntPtr hdc)
         {
             var info = new MonitorInfoEx();
-            _ = NativeMethods.GetMonitorInfo(new HandleRef(null, monitor), info);
+            NativeMethods.GetMonitorInfo(new HandleRef(null, monitor), info);
+
             Bounds = new Rect(
                 info.rcMonitor.Left, info.rcMonitor.Top,
                 info.rcMonitor.Right - info.rcMonitor.Left,
                 info.rcMonitor.Bottom - info.rcMonitor.Top);
+
             WorkingArea = new Rect(
                 info.rcWork.Left, info.rcWork.Top,
                 info.rcWork.Right - info.rcWork.Left,
                 info.rcWork.Bottom - info.rcWork.Top);
+
             IsPrimary = (info.dwFlags & (int)NativeMethods.MonitorDefaultTo.MONITOR_DEFAULTTOPRIMARY) != 0;
             Name = new string(info.szDevice).TrimEnd((char)0);
             Handle = monitor;
@@ -51,15 +53,16 @@ namespace RDPKeepAlive
 
         internal delegate bool MonitorEnumProc(IntPtr monitor, IntPtr hdc, IntPtr lprcMonitor, IntPtr lParam);
 
-        public override bool Equals(object? obj)
+        public override bool Equals(object obj)
         {
-            if (obj is not Monitor) return false;
+            if (obj == null || !(obj is Monitor))
+                return false;
             return Handle == ((Monitor)obj).Handle;
         }
 
-        public bool Equals(Monitor? other)
+        public bool Equals(Monitor other)
         {
-            if (other is null) return false;
+            if (other == null) return false;
             return Handle == other.Handle;
         }
 
@@ -70,28 +73,33 @@ namespace RDPKeepAlive
 
         public override string ToString()
         {
-            return $"Name: {Name} | IsPrimary: {IsPrimary} | Bounds: {Bounds} | WorkingArea: {WorkingArea}";
+            return string.Format("Name: {0} | IsPrimary: {1} | Bounds: {2} | WorkingArea: {3}", Name, IsPrimary, Bounds, WorkingArea);
         }
 
         internal static Rect GetMonitorBounds(IntPtr hWindow)
         {
             var monitorInfoEx = new MonitorInfoEx();
             var hMonitor = GetMonitorHandleFromWindow(hWindow);
-            _ = NativeMethods.GetMonitorInfo(hMonitor, monitorInfoEx);
+            NativeMethods.GetMonitorInfo(hMonitor, monitorInfoEx);
             return monitorInfoEx.rcMonitor;
         }
 
         internal static Monitor GetMonitorFromWindow(IntPtr hWnd)
         {
             var hMonitor = GetMonitorHandleFromWindow(hWnd);
-            return AllMonitors.First(monitor => monitor.Handle == hMonitor.Handle);
+            foreach (var monitor in AllMonitors)
+            {
+                if (monitor.Handle == hMonitor.Handle)
+                    return monitor;
+            }
+            return null;
         }
 
         internal static Rect GetMonitorWorkArea(IntPtr hWindow)
         {
             var monitorInfoEx = new MonitorInfoEx();
             var hMonitor = GetMonitorHandleFromWindow(hWindow);
-            _ = NativeMethods.GetMonitorInfo(hMonitor, monitorInfoEx);
+            NativeMethods.GetMonitorInfo(hMonitor, monitorInfoEx);
             return monitorInfoEx.rcWork;
         }
 
@@ -104,36 +112,26 @@ namespace RDPKeepAlive
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
         internal class MonitorInfoEx
         {
-            [SuppressMessage("Major Code Smell", "S1144:Unused private types or members should be removed", Justification = "<Pending>")]
-            [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "<Pending>")]
             internal int cbSize = Marshal.SizeOf(typeof(MonitorInfoEx));
-
-            internal Rect rcMonitor = new();
-
-            internal Rect rcWork = new();
-
+            internal Rect rcMonitor = new Rect();
+            internal Rect rcWork = new Rect();
             internal int dwFlags = 0;
-
             [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
             internal char[] szDevice = new char[32];
         }
 
         private class MonitorEnumCallback
         {
-            internal ArrayList Monitors { get; }
+            internal ArrayList Monitors { get; private set; }
 
             internal MonitorEnumCallback()
             {
-                Monitors = [];
+                Monitors = new ArrayList();
             }
 
-            [SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "<Pending>")]
-            [SuppressMessage("Major Code Smell", "S1172:Unused method parameters should be removed", Justification = "<Pending>")]
-            [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "<Pending>")]
-            [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
             internal bool Callback(IntPtr monitor, IntPtr hdc, IntPtr lprcMonitor, IntPtr lParam)
             {
-                _ = Monitors.Add(new Monitor(monitor, hdc));
+                Monitors.Add(new Monitor(monitor, hdc));
                 return true;
             }
         }
